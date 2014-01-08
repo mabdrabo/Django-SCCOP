3e281c2f80b928c60ecff5fe0f748afa516bdb96
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-08 18:24:32 +0200

    fix errors

--- Django_SCCOP/sccop/mobile.py
+++ Django_SCCOP/sccop/mobile.py
@@ -3,14 +3,14 @@ from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
 
 from sccop.models import *
+from django.utils import simplejson
 
 # Create your views here.
 
 def master(request):
-	some_data_to_dump = {
+    some_data_to_dump = {
        'some_var_1': 'foo',
        'some_var_2': 'bar',
     }
     data = simplejson.dumps(some_data_to_dump)
-
     return HttpResponse(data, mimetype='application/json')
