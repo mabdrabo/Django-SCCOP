4f000b2d75c51e9423c2c8751e454f477531b4ef
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-08 18:13:15 +0200

    fix for localhost

--- Django_SCCOP/sccop/mobile.py
+++ Django_SCCOP/sccop/mobile.py
@@ -4,8 +4,20 @@ from django.template import RequestContext
 
 from sccop.models import *
 
+from django.utils import simplejson
+from django.core import serializers
+
 # Create your views here.
 
 def master(request):
-	print request
-	return "WOHOOOOO"
+	some_data_to_dump = {
+	   'some_var_1': 'foo',
+	   'some_var_2': 'bar',
+	}
+	data = simplejson.dumps(some_data_to_dump)
+
+	# foos = Foo.objects.all()
+
+	# data = serializers.serialize('json', foos)
+
+	return HttpResponse(data, mimetype='application/json')
