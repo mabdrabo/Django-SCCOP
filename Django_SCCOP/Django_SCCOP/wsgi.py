c064c5f9420ca81d689d3988066212b6a0e1716a
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-08 16:05:13 +0200

    fix for herooku

--- Django_SCCOP/Django_SCCOP/wsgi.py
+++ Django_SCCOP/Django_SCCOP/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_SCCOP.settings")
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+# from django.core.wsgi import get_wsgi_application
+# # from dj_static import Cling
 
-from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
-
-application = get_wsgi_application()
+# application = get_wsgi_application()
 # application = Cling(get_wsgi_application())
