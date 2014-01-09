2e0dc231dd446ee2f3a99f339e809fb71435d631
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-09 18:53:05 +0200

    desperate attempt POST

--- Django_SCCOP/sccop/mobile.py
+++ Django_SCCOP/sccop/mobile.py
@@ -8,6 +8,16 @@ from django.utils import simplejson
 # Create your views here.
 
 def master(request):
+    if request.POST:
+        print "POOSSSTTTT REQUESTTTTT!!!!!!!"
+        some_data_to_dump = {
+           'post_some_var_1': 'foo',
+           'post_some_var_2': 'bar',
+        }
+        data = simplejson.dumps(some_data_to_dump)
+        print request
+        return HttpResponse(data, content_type='application/json')
+
     some_data_to_dump = {
        'some_var_1': 'foo',
        'some_var_2': 'bar',
