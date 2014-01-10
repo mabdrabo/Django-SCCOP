e4b3060832af1a826d66b28a76caf76452773d02
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-10 20:24:01 +0200

    add GET request

--- Django_SCCOP/sccop/mobile.py
+++ Django_SCCOP/sccop/mobile.py
@@ -18,6 +18,19 @@ def master(request):
         print request
         return HttpResponse(data, content_type='application/json')
 
+    if request.GET:
+        print "GEETTTTTT REQUESTTTTT!!!!!!!"
+        print "rpm: ", request.GET['rpm']
+        print "speed: ", request.GET['speed']
+        print "temp: ", request.GET['temp']
+        some_data_to_dump = {
+           'get_some_var_1': 'foo',
+           'get_some_var_2': 'bar',
+        }
+        data = simplejson.dumps(some_data_to_dump)
+        print request
+        return HttpResponse(data, content_type='application/json')
+
     some_data_to_dump = {
        'some_var_1': 'foo',
        'some_var_2': 'bar',
