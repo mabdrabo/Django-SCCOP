8de775e10b7f9b7d7c1b8fb9b8e07090be4f55a5
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-11 23:22:52 +0200

    hopefully better exception reporting

--- Django_SCCOP/sccop/api.py
+++ Django_SCCOP/sccop/api.py
@@ -8,17 +8,20 @@ from django.utils import simplejson
 # Create your views here.
 
 def add(request):
-    if request.GET:
-        print "GEETTTTTT REQUESTTTTT!!!!!!!"
-        print "rpm: ", request.GET['rpm']
-        print "speed: ", request.GET['speed']
-        print "temp: ", request.GET['temp']
-        user = User.objects.get(username=request.GET['username'])
-        LogValue.objects.create(user=user, title="rpm", value=request.GET['rpm'])
-        LogValue.objects.create(user=user, title="speed", value=request.GET['speed'])
-        LogValue.objects.create(user=user, title="temp", value=request.GET['temp'])
+    try:
+        if request.GET:
+            print "GEETTTTTT REQUESTTTTT!!!!!!!"
+            print "rpm: ", request.GET['rpm']
+            print "speed: ", request.GET['speed']
+            print "temp: ", request.GET['temp']
+            user = User.objects.get(username=request.GET['username'])
+            LogValue.objects.create(user=user, title="rpm", value=request.GET['rpm'])
+            LogValue.objects.create(user=user, title="speed", value=request.GET['speed'])
+            LogValue.objects.create(user=user, title="temp", value=request.GET['temp'])
 
-        return HttpResponse("OK")
+            return HttpResponse("OK")
+    except Exception, e:
+        return HttpResponse(e)
 
     # some_data_to_dump = {
     #    'some_var_1': 'foo',
