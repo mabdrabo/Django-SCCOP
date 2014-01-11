29345eac2a8d1c92029871a7ee2dec0f4dc7f5a8
Mahmoud Abdrabo <abdrabo.mahmoud@gmail.com>
2014-01-11 22:42:31 +0200

    ui + logic

--- Django_SCCOP/sccop/models.py
+++ Django_SCCOP/sccop/models.py
@@ -1,30 +1,54 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class User(models.Model):
-	username = models.CharField(max_length=128)
+    username = models.CharField(max_length=128)
+    lon = models.FloatField()
+    lat = models.FloatField()
 
-	def __unicode__(self):
-		return self.username
+    def __unicode__(self):
+        return self.username
 
-	def get_rpm_values(self):
-		return [log.value for log in LogValue.objects.filter(user=self, title='rpm')]
+    def get_values(self, title):
+        return [log.value for log in LogValue.objects.filter(user=self, title=title).order_by('-date')]
 
-	def get_temp_values(self):	# temprature values
-		return [log.value for log in LogValue.objects.filter(user=self, title='temp')]
+    def get_avg(self, title):
+        values = self.get_values(title)
+        if values:
+            return sum(values) / len(values)
+        else:
+            return 0
 
-	def get_speed_values(self):
-		return [log.value for log in LogValue.objects.filter(user=self, title='speed')]
+    def get_max(self, title):
+        values = self.get_values(title)
+        if values:
+            return max(values)
+        else:
+            return 0
 
-	def get_avg_speed(self):
-		values = get_speed_values()
-		return sum(values) / len(values)
+    def get_min(self, title):
+        values = self.get_values(title)
+        if values:
+            return min(values)
+        else:
+            return 0
+
+    def get_location(self):
+        return [self.lat, self.lon]
+
+    def update_location(self, longitude, latitude):
+        self.lon = longitude
+        self.lat = latitude
+        return [self.lat, self.lon]
+
 
 class LogValue(models.Model):
-	user = models.ForeignKey(User, related_name='owner')
-	title = models.CharField(max_length=64)
-	value = models.IntegerField()
+    user = models.ForeignKey(User, related_name='owner')
+    title = models.CharField(max_length=64)
+    value = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
 
-	def __unicode__(self):
-		return self.value
+    def __unicode__(self):
+        return self.title + " " + str(self.value)
--- Django_SCCOP/sccop/views.py
+++ Django_SCCOP/sccop/views.py
@@ -7,13 +7,45 @@ from sccop.models import *
 # Create your views here.
 
 def master(request):
-	return render_to_response('master.html', {}, RequestContext(request))
+    return render_to_response('master.html', {}, RequestContext(request))
+
 
 def dashboard(request, username):
-	if username:
-		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			return render_to_response('dashboard.html', {'msg': 'there is no record with this username'}, RequestContext(request))			
-		return render_to_response('dashboard.html', {'user': user}, RequestContext(request))
-	return render_to_response('dashboard.html', {'msg': 'please enter your username'}, RequestContext(request))
+    if username:
+        try:
+            user = User.objects.get(username=username)
+            values = get_user_values(user)
+        except User.DoesNotExist:
+            print "except"
+            return render_to_response('dashboard.html', {'msg': 'there is no record with this username'}, RequestContext(request))          
+        return render_to_response('dashboard.html', {'user': user, 'values': values}, RequestContext(request))
+    return render_to_response('dashboard.html', {'msg': 'please enter your username'}, RequestContext(request))
+
+
+def logs(request, username):
+    if username:
+        try:
+            user = User.objects.get(username=username)
+            values = get_user_values(username)
+        except User.DoesNotExist:
+            return render_to_response('dashboard.html', {'msg': 'there is no record with this username'}, RequestContext(request))          
+        return render_to_response('dashboard.html', {'user': user, 'values': values}, RequestContext(request))
+    return render_to_response('dashboard.html', {'msg': 'please enter your username'}, RequestContext(request))
+
+
+def get_user_values(user):
+    values = {}
+    values['rpm_values'] = user.get_values('rpm')
+    values['speed_values'] = user.get_values('speed')
+    values['temp_values'] = user.get_values('temp')
+    values['temp_fuel'] = user.get_values('fuel')
+    values['rpm_max'] = user.get_max('rpm')
+    values['rpm_min'] = user.get_min('rpm')
+    values['rpm_avg'] = user.get_avg('rpm')
+    values['speed_max'] = user.get_max('speed')
+    values['speed_min'] = user.get_min('speed')
+    values['speed_avg'] = user.get_avg('speed')
+    values['temp_max'] = user.get_max('temp')
+    values['temp_min'] = user.get_min('temp')
+    values['temp_avg'] = user.get_avg('temp')
+    return values
